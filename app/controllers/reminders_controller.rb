49ebb3f857524444c2a2508ff3a296d37f0e2660
Alexander Lang <alex@upstre.am>
2012-11-09 17:21:52 +0100

    adds editing reminders

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -13,6 +13,10 @@ class RemindersController < ApplicationController
     @reminder = Reminder.new
   end
 
+  def edit
+    @reminder = space.reminders.find params[:id]
+  end
+
   def create
     @reminder = space.reminders.build params[:reminder]
     if @reminder.save
@@ -21,6 +25,18 @@ class RemindersController < ApplicationController
       render 'new'
     end
   end
+
+  def update
+    @reminder = space.reminders.find params[:id]
+    @reminder.attributes = params[:reminder]
+    if @reminder.save
+      redirect_to space_reminders_path(space), notice: 'Reminder changed.'
+    else
+      render 'edit'
+    end
+  end
+
+  private
 
   def space
     @space ||= current_user.spaces.find{|space| space.to_param == params[:space_id]}
---
+++ spec/integration/manage_invoice_reminders_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe 'settings up an invoice reminder' do
+  it 'saves the reminder' do
+    stub_user spaces: ['mutinerie']
+    visit root_path
+    click_link 'Sign in'
+    fill_in 'Subject', with: 'invoice coming'
+    fill_in 'Body', with: 'price: {{ price }}'
+    fill_in 'Days before', with: '4'
+    click_button 'Set Reminder'
+
+    visit space_reminders_path('mutinerie')
+    expect(page).to have_content('invoice coming')
+  end
+end
+
+describe 'editing an invoice reminder' do
+  before(:each) do
+    stub_user spaces: ['mutinerie']
+    visit root_path
+    click_link 'Sign in'
+    Reminder.create!(subject: 'invoice coming', body: '-', days_before: 1) {|reminder|
+      reminder.space_id = 'mutinerie'
+    }
+  end
+
+  it 'changes the reminder' do
+    visit space_reminders_path('mutinerie')
+    click_link 'Edit'
+
+    fill_in 'Subject', with: 'Invoice incoming'
+    click_button 'Change Reminder'
+
+    visit space_reminders_path('mutinerie')
+    expect(page).to have_content('Invoice incoming')
+  end
+end
+
+describe 'deleting an invoice reminder'
