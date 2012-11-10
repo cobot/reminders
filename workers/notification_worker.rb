4d295540bf0494ad6d578d38e48bc5118ad47152
Alexander Lang <alex@upstre.am>
2012-11-10 15:08:57 +0100

    improves error message

--- workers/notification_worker.rb
+++ workers/notification_worker.rb
@@ -2,5 +2,5 @@ require 'httparty'
 response = HTTParty.post params[:callback_url]
 
 unless response.success?
-  raise response.body.to_s
+  raise "API call error. Status: #{response.code}. Body: #{response.body.to_s.inspect}"
 end
