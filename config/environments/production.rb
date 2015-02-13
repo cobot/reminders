b17230bbaf964553d32a8d3d757dd9cc19f9eaee
Alexander Lang <alex@upstre.am>
2015-02-13 13:00:36 +0100

    adds not logging requests to the home page

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,6 +33,7 @@ Reminders::Application.configure do
   # See everything in the log (default is :info)
   # config.log_level = :debug
   config.lograge.enabled = true
+  config.lograge.ignore_actions = ['session#new']
 
   # Prepend all log lines with the following tags
   # config.log_tags = [ :subdomain, :uuid ]
