5eb8bc14895bbbef0597f6c6e100bc86e37dd151
Dera Luce <deraluce@gmail.com>
2019-03-14 16:03:11 +0100

    Change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Reminders::Application.routes.draw do
+  #root 'welcome#show'
+
   get '/auth/:provider/callback', to: 'sessions#create'
+  get '/auth/failure', to: 'sessions#failure'
 
   root to: 'sessions#new'
   resource :session, only: :destroy
